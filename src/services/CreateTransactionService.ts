188ebdff3007603320e8e9e0436682ecf4657ed5
Murilo Campos <murilo13campos@gmail.com>
2020-04-17 20:46:33 -0400

    creating transaction and almost listing the transactions

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,60 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository, getRepository } from 'typeorm';
+
+import AppError from '../errors/AppError';
 
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+import TransactionRepository from '../repositories/TransactionsRepository';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionsRepository = getCustomRepository(TransactionRepository);
+    const categoriesRepository = getRepository(Category);
+    let category_id;
+
+    const balance = await transactionsRepository.getBalance();
+
+    if (value > balance.total && type === 'outcome') {
+      throw new AppError("You don't have enough credit for that transaction");
+    }
+
+    const categoryExists = await categoriesRepository.findOne({
+      where: { title: category },
+    });
+
+    if (!categoryExists) {
+      const newCategory = categoriesRepository.create({
+        title: category,
+      });
+
+      await categoriesRepository.save(newCategory);
+      category_id = newCategory.id;
+    } else {
+      category_id = categoryExists.id;
+    }
+
+    const newTransaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+      category_id,
+    });
+
+    await transactionsRepository.save(newTransaction);
+
+    return newTransaction;
   }
 }
 
